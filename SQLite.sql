f569be1f4c25386210d6a887890fb9f9ff21bdb7
DEV Ed.006 <122188281+DEV-Cartisa@users.noreply.github.com>
2023-09-12 06:41:27 +0530

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,17 @@
+CREATE TABLE applestore_description_combined AS
+
+SELECT * FROM appleStore_description1
+
+UNION ALL 
+
+SELECT * FROM appleStore_description2
+
+UNION ALL 
+
+SELECT * FROM appleStore_description3
+
+UNION ALL 
+
+SELECT * FROM appleStore_description4
+
+ 
